e09ed9f42ea8bab04542ba15100324c7db0540c2
Andreas Alptun <andreas.alptun@gmail.com>
2020-02-20 11:18:54 +0100

    primitive types long and long long defined

--- include/dukglue/detail_primitive_types.h
+++ include/dukglue/detail_primitive_types.h
@@ -39,13 +39,14 @@ namespace dukglue {
 		DUKGLUE_SIMPLE_VALUE_TYPE(uint8_t,  duk_is_number, duk_get_uint, duk_push_uint, value)
 		DUKGLUE_SIMPLE_VALUE_TYPE(uint16_t, duk_is_number, duk_get_uint, duk_push_uint, value)
 		DUKGLUE_SIMPLE_VALUE_TYPE(uint32_t, duk_is_number, duk_get_uint, duk_push_uint, value)
-		DUKGLUE_SIMPLE_VALUE_TYPE(uint64_t, duk_is_number, duk_get_number, duk_push_number, value) // have to cast to double
 		DUKGLUE_SIMPLE_VALUE_TYPE(unsigned long, duk_is_number, duk_get_number, duk_push_number, value) // have to cast to double
+		DUKGLUE_SIMPLE_VALUE_TYPE(unsigned long long, duk_is_number, duk_get_number, duk_push_number, value) // have to cast to double
 
 		DUKGLUE_SIMPLE_VALUE_TYPE(int8_t,  duk_is_number, duk_get_int, duk_push_int, value)
 		DUKGLUE_SIMPLE_VALUE_TYPE(int16_t, duk_is_number, duk_get_int, duk_push_int, value)
 		DUKGLUE_SIMPLE_VALUE_TYPE(int32_t, duk_is_number, duk_get_int, duk_push_int, value)
-		DUKGLUE_SIMPLE_VALUE_TYPE(int64_t, duk_is_number, duk_get_number, duk_push_number, value) // have to cast to double
+		DUKGLUE_SIMPLE_VALUE_TYPE(long, duk_is_number, duk_get_number, duk_push_number, value) // have to cast to double
+		DUKGLUE_SIMPLE_VALUE_TYPE(long long, duk_is_number, duk_get_number, duk_push_number, value) // have to cast to double
 
 		// signed char and unsigned char are surprisingly *both* different from char, at least in MSVC
 		DUKGLUE_SIMPLE_VALUE_TYPE(char, duk_is_number, duk_get_int, duk_push_int, value)
